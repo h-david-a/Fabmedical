181c510af22a237d1653b1bcd3aaa6f2990679d8
h-david-a <h.david.a@gmail.com>
2022-03-24 14:44:53 +0000

    Added Application Insights

--- content-web/app.js
+++ content-web/app.js
@@ -5,6 +5,10 @@ const request = require('request');
 
 const app = express();
 
+const appInsights = require("applicationinsights");
+appInsights.setup("a2eeb0b3-6937-4255-be23-6efcd1cbc296");
+appInsights.start();
+
 app.use(express.static(path.join(__dirname, 'dist/content-web')));
 const contentApiUrl = process.env.CONTENT_API_URL || "http://localhost:3001";
 
